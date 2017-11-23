61a7bed32b79d983d4fd962d92ac15a9b2545a07
martinderm <martin@convive.io>
2017-11-23 15:49:18 +0100

    fixes

--- send_mail.php
+++ send_mail.php
@@ -1,7 +1,7 @@
 <?php
 // Fetching Values from URL.
 $lastname = $_POST['lastname'];
-$fristname = $_POST['firstname'];
+$firstname = $_POST['firstname'];
 $street = $_POST['street'];
 $number = $_POST['number'];
 $postcode = $_POST['postcode'];
@@ -19,30 +19,30 @@ $subject = "Spende an Mosaik";
 // To send HTML mail, the Content-type header must be set.
 $headers = 'MIME-Version: 1.0' . "\r\n";
 $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-$headers .= 'From:' . 'spende@mosaik-blog.at'  . "\r\n"; // Sender's Email
+$headers .= 'From:' . 'spenden@mosaik-blog.at'  . "\r\n"; // Sender's Email
 /*$headers .= 'Cc:' . $email. "\r\n"; // Carbon copy to Sender*/
-$template = '<div style="padding:50px; color:white;">Hello ' . $name . ',<br/>'
+$template = '<div style="padding:50px;">Hallo! ' . $name . '<br/>'
 . '<br/>Neue Spende<br/><br/>'
-. 'Name:' . $lastname . ' ' . $firstname . '<br/>'
-. 'Straße:' . $street . '<br/>'
-. 'Nummer:' . $number . '<br/>'
-. 'Postleitzahl:' . $postcode . '<br/>'
-. 'Ort:' . $residence . '<br/>'
-. 'E-Mail:' . $email . '<br/>'
-. 'Newsletter:' . $newsletter .'<br/>'
-. 'Telefon:' . $phone . '<br/>'
-. 'Bank:' . $bank . '<br/>'
-. 'IBAN:' . $iban . '<br/>'
-. 'BIC:' . $bic . '<br/>'
-. 'Intervall' . $interval . '<br/>'
-. 'Betrag' . $amount
+. 'Name: ' . $lastname . ' ' . $firstname . '<br/>'
+. 'Straße: ' . $street . '<br/>'
+. 'Nummer: ' . $number . '<br/>'
+. 'Postleitzahl: ' . $postcode . '<br/>'
+. 'Ort: ' . $residence . '<br/>'
+. 'E-Mail: ' . $email . '<br/>'
+. 'Newsletter: ' . $newsletter .'<br/>'
+. 'Telefon: ' . $phone . '<br/>'
+. 'Bank: ' . $bank . '<br/>'
+. 'IBAN: ' . $iban . '<br/>'
+. 'BIC: ' . $bic . '<br/>'
+. 'Intervall (Spende all x Monate): ' . $interval . '<br/>'
+. 'Betrag: ' . $amount
 . '<br/>'
 . 'Vielen Dank für deine Spende.</div>';
-$sendmessage = "<div style=\"background-color:#7E7E7E; color:white;\">" . $template . "</div>";
+$sendmessage = "<div>" . $template . "</div>";
 // Message lines should not exceed 70 characters (PHP rule), so wrap it.
 $sendmessage = wordwrap($sendmessage, 70);
 // Send mail by PHP Mail Function.
-mail("martin@convive.io", $subject, $sendmessage, $headers);
-echo "Danke für Ihre Spende! Bitte speihcern Sie das generierte PDF für Ihre Unterlagen.";
+mail("spenden@mosaik-blog.at", $subject, $sendmessage, $headers);
+echo "Danke für Ihre Spende! Bitte speichern Sie das generierte PDF für Ihre Unterlagen.";
 
 ?>
